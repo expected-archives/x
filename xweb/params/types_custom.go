052ca9f7d56bf5502c6d6371fd3df9844818cce2
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2023-09-03 18:02:43 +0200

    params: add missing files

---
+++ xweb/params/types_custom.go
@@ -0,0 +1,14 @@
+package params
+
+import (
+	"reflect"
+	"time"
+)
+
+var d = time.Duration(1)
+
+// Custom is the list of custom types supported.
+var Custom = []reflect.Type{
+	reflect.TypeOf(time.Duration(1)),
+	reflect.TypeOf(&d),
+}
